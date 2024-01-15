213e1ade6810b7782074a67889df7265b2ec8094
bodie <bodo.braegger@pm.me>
2024-01-15 01:03:33 +0100

    feat: better visibility of years in post list

--- src/components/ListPosts.vue
+++ src/components/ListPosts.vue
@@ -62,7 +62,7 @@ function getGroupName(p: Post) {
           '--enter-step': '60ms',
         }"
       >
-        <span text-8em color-transparent absolute left--3rem top--2rem font-bold text-stroke-2 text-stroke-hex-aaa op10>{{ getGroupName(route) }}</span>
+        <span text-8em color-transparent absolute left--3rem top--2rem font-bold text-stroke-2 text-stroke-hex-aaa op20>{{ getGroupName(route) }}</span>
       </div>
       <div
         class="slide-enter"
