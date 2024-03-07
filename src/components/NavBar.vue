8d64ae2c87de1780290553bb69ebbb518775c1ce
bodie <bodo.braegger@pm.me>
2024-03-07 16:27:37 +0100

    feat: fix artsy links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -24,7 +24,7 @@ const { y: scroll } = useWindowScroll()
     <nav class="nav">
       <div class="left">
         <RouterLink
-          class="logo-wrap select-none outline-none font-mono"
+          class="select-none outline-none font-mono w-fit hidden sm:block b-dashed b-1 router-link-exact-active"
           to="/"
           focusable="false"
         >
@@ -38,13 +38,13 @@ const { y: scroll } = useWindowScroll()
         <RouterLink class="font-mono" to="/" title="Home">
           <span>Home</span>
         </RouterLink>
-        <RouterLink class="font-mono" to="/projects" title="Projects">
+        <RouterLink id="projects" class="font-mono" to="/projects" title="Projects">
           <span>Projects</span>
         </RouterLink>
-        <RouterLink class="font-mono" to="/notes" title="Blog">
+        <RouterLink id="notes" class="font-mono" to="/notes" title="Blog">
           <span>Notes</span>
         </RouterLink>
-        <a class="select-none" href="https://github.com/bodobraegger" target="_blank" title="GitHub">
+        <a id="github" class="select-none" href="https://github.com/bodobraegger" target="_blank" title="GitHub">
           <div i-uil-github-alt />
         </a>
         <ToggleTheme />
@@ -74,9 +74,9 @@ const { y: scroll } = useWindowScroll()
   display: grid;
   grid-template-columns: auto max-content;
   box-sizing: border-box;
-  --fg: rgba(125, 125, 125, 0.4);
-  --fg-deep: rgba(125, 125, 125, 0.7);
-  --fg-deeper: rgba(125, 125, 125, 1);
+  --fg: rgba(201, 201, 201, 0.4);
+  --fg-deep: rgba(201, 201, 201, 0.7);
+  --fg-deeper: rgba(201, 201, 201, 1);
 }
 
 .nav > *:not(:first-child) {
@@ -97,19 +97,20 @@ const { y: scroll } = useWindowScroll()
 }
 
 .nav a:not(.select-none) {
-  border: 1px dashed var(--fg);
+  border: 1px dashed var(--fg-deep);
 }
 
 .nav a:hover {
   opacity: 1;
+  border-style: solid !important;
   text-decoration-color: inherit;
 }
 
-.nav a.router-link-active {
+.nav a.router-link-active, .nav a.router-link-exact-active {
   opacity: 1;
   /* text-decoration: wavy underline; */
   /* text-decoration-skip-ink: none; */
-  border: 1px solid var(--fg-deep);
+  border: 1px dashed var(--fg-deeper);
 }
 
 .nav .right {
