f18deedc5df201702b9b7889465eac0041bb1719
bodie <bodo.braegger@pm.me>
2024-03-26 20:09:43 +0100

    feature: make hydraize a type of post for now, move hydra loading to wrapperpost

--- src/components/WrapperPost.vue
+++ src/components/WrapperPost.vue
@@ -1,4 +1,5 @@
 <script setup lang='ts'>
+import { useScriptTag } from '@vueuse/core';
 import { formatDate } from '~/logics'
 
 const { frontmatter } = defineProps({
@@ -76,6 +77,126 @@ onMounted(() => {
       setTimeout(navigate, 1000)
   }, 1)
 })
+
+
+if(frontmatter.hydra) {
+    console.log('hydra is true')
+    const codeMirrorAddOns = [
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/mode/javascript/javascript.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/addon/hint/javascript-hint.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/addon/hint/show-hint.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/addon/selection/mark-selection.min.js",
+        "https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/addon/comment/comment.min.js",
+    ]
+
+
+    // useScriptTag("https://unpkg.com/torus-dom/dist/index.min.js",
+    // () => {
+    // console.log('torus loaded');
+    // useScriptTag("https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.59.2/codemirror.min.js",
+    //     () => {
+    //         console.log('codemirror loaded');
+
+    //         for (const script of codeMirrorAddOns) {
+    //             useScriptTag(script, () => {
+    //                 console.log(`${script} loaded`);
+    //             }, {
+    //                 async: true,
+    //                 defer: true,
+    //             });
+    //         }
+    // });
+
+    useScriptTag("https://hyper-hydra.glitch.me/hydra-arrays.js", () => {
+        console.log('hydra-arrays loaded');
+    }, {
+        async: true,
+    });
+    
+    useScriptTag('https://unpkg.com/hydra-synth',
+        () => {
+            console.log('hydra-synth loaded');
+            let hydra, hydraCanvas;
+            hydraCanvas = document.createElement("canvas");
+            let width = 650-16*2;
+            let height = width;
+            hydraCanvas.width = width;
+            hydraCanvas.height = height;
+            hydraCanvas.id = "hydraCanvas";
+            const placeholders = [];
+
+            hydra = new Hydra({
+                canvas: hydraCanvas,
+                detectAudio: false,
+                enableStreamCapture: false,
+                width: width,
+                height: height,
+            });
+
+            const codeBlocks = document.querySelectorAll('pre:has(.language-javascript)')
+            codeBlocks.forEach((preEl) => {
+                // const parentEl = preEl.parentElement
+                preEl.classList += ' grid overflow-x-auto';
+                const codeEl = preEl.firstChild
+                codeEl.classList += " row-start-1 col-start-1 z-1 min-h-618px min-w-618px hover:cursor-pointer"
+
+                const placeholder = document.createElement('div');
+                placeholder.classList += "hydracontainer row-start-1 col-start-1 z-0";
+                placeholders.push(placeholder);
+                preEl.insertAdjacentElement('beforeend', placeholder)
+                
+                const linkEl = document.createElement('a')
+                linkEl.href = `https://hydra.ojack.xyz/?code=${btoa(encodeURIComponent(codeEl.textContent))}`
+                linkEl.target = "_blank"
+                linkEl.textContent = "open in hydra"
+                linkEl.classList += "artwork-link"
+                preEl.children[1].insertAdjacentElement('afterend', linkEl)
+
+                preEl.onfocus = () => {
+                    // console.log('focusing')
+                    hush();
+                    solid(0,0,0,0).out(o0)
+                    solid(0,0,0,0).out(o1)
+                    solid(0,0,0,0).out(o2)
+                    solid(0,0,0,0).out(o3)
+                    render(o0);
+                    setTimeout(()=>{
+                        eval(codeEl.textContent);
+                        // console.log('evaluated, rendering, and waiting for 60ms');
+                    }, 60);
+                    placeholder.appendChild(hydraCanvas);
+                    // make text semi transparent
+                    codeEl.classList.add('op-50');
+                    // add black background
+                    placeholder.classList.add('bg-black!');
+                }
+                preEl.onfocusout = () => {
+                    // remove black background
+                    codeEl.classList.remove('op-50');
+                    placeholder.classList.remove('bg-black!');
+                }
+
+                var observer = new IntersectionObserver(function (entries) {
+                if (entries[0].isIntersecting === true) {
+                    // console.log('intersecting');
+                    preEl.onfocus();
+                } else {
+                    // console.log('not intersecting');
+                    preEl.onfocusout();
+                }
+                }, { threshold: [1], rootMargin: "0% 100% 0% 100%"});
+                observer.observe(preEl);
+            })
+            window.onmessage = e => {
+                console.log(e)
+            }
+        },
+        { async: true, defer: true,}
+    );
+    //     },
+    //     { async: true, defer: true, }
+    // )
+  }
 </script>
 
 <template>
