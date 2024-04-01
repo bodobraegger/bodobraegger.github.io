611a4aab35856bbebd4043dc1dcf9bd013cddb22
bodie <bodo.braegger@pm.me>
2024-04-01 14:32:31 +0200

    fix: cc link spacing

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,8 +2,7 @@
   <footer class="mt-10 mb-6 prose m-auto flex slide-enter animate-delay-1200! mt-auto px-7 md-px-0 w-full ">
     <span class="text-sm op50">
       <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">
-        CC BY-NC-SA 4.0
-      </a> 
+        CC BY-NC-SA 4.0</a>
       2023-PRESENT ©&nbsp;Bodo&nbsp;Braegger 
     </span>
     <div class="flex-auto" />
