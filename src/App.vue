ed24db93c20f26e13abdd15ee4cf8888cf144e7e
bodobraegger <braegger.bodo@gmail.com>
2025-01-30 14:13:44 +0100

    feat: add naive decapcms

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,17 @@ const route = useRoute()
 </script>
 
 <template>
-  <div class="flex flex-col justify-start min-h-screen xl-mx-auto xl:max-w-500">
-    <NavBar />
-    <main class="px-7 py-10 of-x-hidden">
-      <RouterView />
-    </main>
-    <Footer :key="route.path" />
+  <ClientOnly v-if="route.path === '/admin'">
+    <DecapCMS />
+  </ClientOnly>
+  <ClientOnly v-else>
+    <div class="flex flex-col justify-start min-h-screen xl-mx-auto xl:max-w-500">
+      <NavBar />
+      <main class="px-7 py-10 of-x-hidden">
+        <RouterView />
+      </main>
+      <Footer :key="route.path" />
   </div>
   <Lightbox/>
+</ClientOnly>
 </template>
---
+++ src/components/DecapCMS.vue
@@ -0,0 +1,9 @@
+<script setup lang='ts'>
+import { useScriptTag } from '@vueuse/core';
+
+const script = useScriptTag(
+  'https://unpkg.com/decap-cms@^3.0.0/dist/decap-cms.js',
+  () => {
+    console.log('decap-cms loaded');
+  });
+</script>
