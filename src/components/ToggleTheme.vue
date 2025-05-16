e3fa171055023e5ffaca724c3a05377de2067014
bodobraegger <braegger.bodo@gmail.com>
2025-05-16 12:16:15 +0200

    feat: text for theme toggle

--- src/components/ToggleTheme.vue
+++ src/components/ToggleTheme.vue
@@ -4,6 +4,8 @@ import { toggleDark } from '~/logics'
 
 <template>
   <a class="select-none" title="Toggle Color Scheme" @click="toggleDark">
-    <div i-ri-sun-line dark:i-ri-moon-line />
+    <!-- <div i-ri-sun-line dark:i-ri-moon-line /> -->
+    <span class="dark:hidden text-xs">dark?</span>
+    <span class="hidden dark:inline text-xs">light?</span>
   </a>
 </template>
