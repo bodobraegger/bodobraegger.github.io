3b25e780f079b80d17da4f8c36c5a74a18fb20f0
bodobraegger <braegger.bodo@gmail.com>
2025-05-19 16:10:55 +0200

    feat: blur mask footer also

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
-  <footer class="mt-10 mb-6 prose m-auto flex mt-auto px-7 md-px-0 w-full ">
+  <footer class="pt-10 pb-6 prose m-auto flex mt-auto px-7 md-px-0 w-full backdrop-blur-sm ">
     <span class="text-xs font-light op50">
       <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/" style="color:inherit">
         CC BY-NC-SA 4.0</a>
-      2023-PRESENT ©&nbsp;Bodo&nbsp;Braegger 
+      2023-PRESENT ©&nbsp;Bodo&nbsp;Braegger
     </span>
   </footer>
 </template>
+
+<style lang="css" scoped>
+footer {
+  mask-image: linear-gradient(to top, var(--c-bg) 50%, transparent 100%);
+}
+</style>
