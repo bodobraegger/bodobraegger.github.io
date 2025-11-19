8263992d4f6adfbf0459a00312ad453a17a7c0a1
bodobraegger <bodo.braegger@gess.ethz.ch>
2025-11-19 12:50:16 +0100

    feat: gradient bg

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ const route = useRoute()
 </script>
 
 <template>
+  <div class="page-background" />
   <div class="flex flex-col justify-start min-h-screen xl-mx-auto xl:max-w-500">
     <NavBar />
     <main class="px-7 py-10 of-x-hidden">
@@ -10,5 +11,5 @@ const route = useRoute()
     </main>
     <Footer :key="route.path" />
   </div>
-  <Lightbox/>
+  <Lightbox />
 </template>
