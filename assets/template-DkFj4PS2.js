7ab6720f7451099e7c07213d22c893ac6f4e3455
bodobraegger <bodobraegger@users.noreply.github.com>
2026-09-14 19:03:04 +0000

    deploy: 71ed81332434384582f82094b2b741e686050d1f

---
+++ assets/template-DkFj4PS2.js
@@ -0,0 +1,1 @@
+import{u as t}from"./app-CuDUZYfO.js";import{f as e,b as a,E as o}from"./vue-vendor-owAXO5xd.js";const s={class:"prose m-auto"},p={__name:"template",setup:p=>(t({title:"...",meta:[{property:"og:title",content:"..."},{name:"twitter:title",content:"..."}]}),(t,p)=>(o(),e("div",s,[...p[0]||(p[0]=[a("p",null,"…",-1)])])))};export{p as default};
